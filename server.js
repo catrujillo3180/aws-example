26b963fc6abb9e5fc81f5a2bb082db4ee711a60a
Cesar_Trujillo <ca.trujillo3180@gmail.com>
2021-04-25 14:12:08 -0500

    port updated

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = express();
 
 app.get('/', (req, res) => res.send('<h1> Express with HTML</h1>'))
 
-app.listen(3000);
-console.log('Server on port 3000')
+app.listen(8080);
+console.log('Server on port 8080')
